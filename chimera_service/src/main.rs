ca4007aa443bfe82b6779f48544be35f0b374064
MFSGA <chimera_rust@proton.me>
2026-02-23 00:45:39 +0800

    feat: add register_ctrlc_handler

--- chimera_service/src/main.rs
+++ chimera_service/src/main.rs
@@ -1,12 +1,16 @@
 // #![feature(error_generic_member_access)]
 // #![feature(exit_status_error)]
 
+mod utils;
+
 pub mod consts;
 
 use consts::ExitCode;
 
+use utils::os::register_ctrlc_handler;
+
 fn main() -> ExitCode {
-    // let mut rx = register_ctrlc_handler();
+    let mut rx = register_ctrlc_handler();
     // register_panic_hook();
     todo!()
 }
---
+++ start.ps1
@@ -0,0 +1,1 @@
+cargo watch -x "run --package chimera-service --bin chimera-service"
