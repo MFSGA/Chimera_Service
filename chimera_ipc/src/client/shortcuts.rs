2e25648248717e018df1a03e741384abeb1de1b3
MFSGA <chimera_rust@proton.me>
2026-04-07 17:58:08 +0800

    feat(specta): prepared the feature for gui

--- chimera_ipc/src/client/shortcuts.rs
+++ chimera_ipc/src/client/shortcuts.rs
@@ -1,8 +1,9 @@
 use std::{borrow::Cow, sync::OnceLock};
 
+use axum::body::Body;
 use bytes::Bytes;
 use http_body_util::Empty;
-use hyper::Request;
+use hyper::{Request, header::CONTENT_TYPE};
 
 use crate::{SERVICE_PLACEHOLDER, api, client::send_request};
 
@@ -33,5 +34,18 @@ impl<'a> Client<'a> {
             .ok()?;
         let data = response.data.unwrap();
         Ok(data)
+    }
+
+    pub async fn start_core(&self, payload: &api::core::start::CoreStartReq<'_>) -> Result<'_, ()> {
+        let payload = simd_json::serde::to_string(payload)?;
+        let request = Request::post(api::core::start::CORE_START_ENDPOINT)
+            .header(CONTENT_TYPE, "application/json")
+            .body(Body::from(payload))?;
+        let response = send_request(&self.0, request)
+            .await?
+            .cast_body::<api::core::start::CoreStartRes>()
+            .await?;
+        response.ok()?;
+        Ok(())
     }
 }
--- chimera_ipc/src/types/status.rs
+++ chimera_ipc/src/types/status.rs
@@ -4,6 +4,7 @@ use std::borrow::Cow;
 
 #[derive(Debug, Serialize, Deserialize, PartialEq, Clone, Copy, Eq)]
 #[serde(rename_all = "snake_case")]
+#[cfg_attr(feature = "specta", derive(specta::Type))]
 pub enum ServiceStatus {
     NotInstalled,
     Stopped,
@@ -11,9 +12,10 @@ pub enum ServiceStatus {
 }
 
 #[derive(Debug, Serialize, Deserialize)]
+#[cfg_attr(feature = "specta", derive(specta::Type))]
 pub struct StatusInfo<'n> {
-    pub name: Cow<'n, str>,
-    pub version: Cow<'n, str>,
+    pub name: Cow<'n, str>,    // The client program name
+    pub version: Cow<'n, str>, // The client program version
     pub status: ServiceStatus,
     pub server: Option<StatusResBody<'n>>,
 }
