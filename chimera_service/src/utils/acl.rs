61ea08b4250703699e4831813298d70f8ee234c2
MFSGA <chimera_rust@proton.me>
2026-04-08 17:23:06 +0800

    feat(acl): for windows security

--- chimera_service/src/utils/acl.rs
+++ chimera_service/src/utils/acl.rs
@@ -1,12 +1,61 @@
+use std::{os::windows::ffi::OsStrExt, path::Path};
+
+use anyhow::Context;
 use tokio::{
     fs::{File, OpenOptions},
     io::{AsyncReadExt, AsyncWriteExt},
+};
+use windows::{
+    Win32::{
+        Foundation::*,
+        Security::{
+            Authorization::{
+                ConvertStringSecurityDescriptorToSecurityDescriptorW, SDDL_REVISION_1,
+            },
+            DACL_SECURITY_INFORMATION, PSECURITY_DESCRIPTOR, SetFileSecurityW,
+        },
+    },
+    core::{Free, PCWSTR},
 };
 
 const ACL_FILE_NAME: &str = "acl.list";
 
 fn acl_path() -> std::path::PathBuf {
     crate::utils::dirs::service_config_dir().join(ACL_FILE_NAME)
+}
+
+fn set_file_acl_from_sddl(file: &Path, sddl: &str) -> anyhow::Result<()> {
+    let file_path_wide: Vec<u16> = file.as_os_str().encode_wide().chain(Some(0)).collect();
+    let sddl_wide: Vec<u16> = sddl.encode_utf16().chain(Some(0)).collect();
+
+    let mut security_descriptor_ptr = PSECURITY_DESCRIPTOR::default();
+    let mut sd_size = 0;
+
+    unsafe {
+        ConvertStringSecurityDescriptorToSecurityDescriptorW(
+            PCWSTR::from_raw(sddl_wide.as_ptr()),
+            SDDL_REVISION_1,
+            &mut security_descriptor_ptr,
+            Some(&mut sd_size),
+        )
+        .context("failed to convert sddl to security descriptor")?;
+    }
+
+    unsafe {
+        let result = SetFileSecurityW(
+            PCWSTR::from_raw(file_path_wide.as_ptr()),
+            DACL_SECURITY_INFORMATION,
+            security_descriptor_ptr,
+        );
+
+        HLOCAL(security_descriptor_ptr.0).free();
+
+        if !result.as_bool() {
+            anyhow::bail!("failed to set file acl");
+        }
+    }
+
+    Ok(())
 }
 
 pub async fn create_acl_file() -> Result<(), anyhow::Error> {
@@ -19,7 +68,13 @@ pub async fn create_acl_file() -> Result<(), anyhow::Error> {
         tokio::fs::create_dir_all(parent).await?;
     }
 
-    File::create(path).await?;
+    let sddl =
+        chimera_ipc::utils::acl::generate_windows_security_descriptor::<&str>(&[], None, None)
+            .context("failed to generate sddl")?;
+    File::create(&path)
+        .await
+        .context("failed to create acl file")?;
+    set_file_acl_from_sddl(&path, &sddl)?;
     Ok(())
 }
 
@@ -29,15 +84,22 @@ pub async fn read_acl_file() -> Result<Vec<String>, anyhow::Error> {
         return Ok(vec![]);
     }
 
-    let mut file = File::open(path).await?;
-    let mut content = String::with_capacity(256);
-    file.read_to_string(&mut content).await?;
+    let mut file = File::open(path).await.context("failed to open acl file")?;
+    let mut content = String::with_capacity(4096);
+    file.read_to_string(&mut content)
+        .await
+        .context("failed to read acl file")?;
 
     Ok(content
         .lines()
-        .map(str::trim)
-        .filter(|line| !line.is_empty())
-        .map(ToOwned::to_owned)
+        .filter_map(|line| {
+            let line = line.trim();
+            if line.starts_with("S-") {
+                Some(line.to_string())
+            } else {
+                None
+            }
+        })
         .collect())
 }
 
@@ -47,7 +109,8 @@ pub async fn write_acl_file<T: AsRef<str>>(entries: &[T]) -> Result<(), anyhow::
         .write(true)
         .truncate(true)
         .open(path)
-        .await?;
+        .await
+        .context("failed to open acl file")?;
     file.write_all(
         entries
             .iter()
@@ -56,6 +119,7 @@ pub async fn write_acl_file<T: AsRef<str>>(entries: &[T]) -> Result<(), anyhow::
             .join("\n")
             .as_bytes(),
     )
-    .await?;
+    .await
+    .context("failed to write acl file")?;
     Ok(())
 }
